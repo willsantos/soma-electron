082cda6b4b30fe0fcc500a79baed5998e4e7081a
Wilson Santos <contato@wilsonsantos.net>
2020-12-02 11:45:30 -0300

    Create routes and Theme Colors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import {ThemeProvider, createMuiTheme} from '@material-ui/core';
 import './App.css';
+import {Route, Switch, HashRouter as Router} from 'react-router-dom';
+import AppMenu from './components/AppMenu';
+import Login from './pages/Login';
+import RegisterPage from './pages/RegisterPage';
+import ClientsPage from './pages/ClientsPage';
+
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      /* main:'#004d40' */
+      main: '#4e342e'
+    },
+    secondary:{
+      main:'#004d40'
+    }
+  },
+  typography:{
+    fontSize:20
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppMenu/>
+      <Router>
+        <Switch>
+          <Route path="/" component={Login} exact/>
+          <Route path="/register" component={RegisterPage}/>
+          <Route path="/clients" component={ClientsPage}/>
+        </Switch>
+      </Router>
+
+      
+
+    </ThemeProvider>
   );
 }
 
