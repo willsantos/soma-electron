ab6720a7aad41e74d25a9e775226d65cb233289b
Wilson Santos <contato@wilsonsantos.net>
2020-12-02 11:48:03 -0300

    Add Register Page

---
+++ src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom';
+const RegisterPage = () =>{
+  return(
+    <>
+    <h1>RegisterPage</h1>
+    <Link to="/" >Voltar</Link>
+    </>
+  );
+}
+
+export default RegisterPage;
